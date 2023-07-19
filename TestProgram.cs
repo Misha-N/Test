56c7f6f5fc42865ebddd4777a5a6f5e891b423d9
Michal Frýba <54448758+Misha-N@users.noreply.github.com>
2023-07-19 20:29:20 +0200

    Create TestProgram.cs

---
+++ TestProgram.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is a test program running in memory!");
+            Console.ReadLine();
+        }
+    }
+}
